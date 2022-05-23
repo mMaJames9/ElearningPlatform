3535ee5c864386976335287f1f87542a00f6a114
mMaJames9 <anthonymisse85@gmail.com>
2022-05-24 00:59:16 +0100

    Retrait du fichier .sqlite du .gitignore

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,61 @@
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<!-- plugins:css -->
+<link rel="stylesheet" href="{{ asset('vendors/feather/feather.css') }}">
+<link rel="stylesheet" href="{{ asset('vendors/mdi/css/materialdesignicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendors/ti-icons/css/themify-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('vendors/typicons/typicons.css') }}">
+<link rel="stylesheet" href="{{ asset('vendors/simple-line-icons/css/simple-line-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.base.css') }}">
+<!-- endinject -->
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+<!-- Plugin css for this page -->
+<link rel="stylesheet" href="{{ asset('vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+<link rel="stylesheet" href="{{ asset('js/select.dataTables.min.css') }}">
+<!-- End plugin css for this page -->
 
-        @livewireStyles
+<!-- inject:css -->
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/vertical-layout-light/style.css') }}">
+<!-- endinject -->
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
+<link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">
+
+<!-- plugins:js -->
+<script src="{{ asset('vendors/js/vendor.bundle.base.js') }}" defer></script>
+<script src="{{ asset('vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}" defer></script>
+<!-- endinject -->
+
+ <!-- Plugin js for this page -->
+ <script src="{{ asset('vendors/chart.js/Chart.min.js') }}" defer></script>
+ <script src="{{ asset('vendors/progressbar.js/progressbar.min.js') }}" defer></script>
+ <!-- End plugin js for this page -->
+
+<!-- inject:js -->
+<script src="{{ mix('js/app.js') }}" defer></script>
+<script src="{{ asset('js/off-canvas.js') }}" defer></script>
+<script src="{{ asset('js/hoverable-collapse.js') }}" defer></script>
+<script src="{{ asset('js/template.js') }}" defer></script>
+<script src="{{ asset('js/settings.js') }}" defer></script>
+<script src="{{ asset('js/todolist.js') }}" defer></script>
+<!-- end-->
+
+<!-- Custom js for this page-->
+<script src="{{ asset('js/jquery.cookie.js') }}" defer></script>
+<script src="{{ asset('js/dashboard.js') }}" defer></script>
+<script src="{{ asset('js/Chart.roundedBarCharts.js') }}" defer></script>
+<!-- End custom js for this page-->
+
+</head>
     <body class="font-sans antialiased">
         <x-jet-banner />
 
