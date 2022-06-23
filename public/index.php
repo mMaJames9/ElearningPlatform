a356ad21e0f105d6e1d9887d5056967363c2a785
mMaJames9 <anthonymisse85@gmail.com>
2022-06-23 18:29:43 +0100

    MAJ Hostinger

--- public/index.php
+++ public/index.php
@@ -20,6 +20,10 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+// if (file_exists($maintenance = __DIR__.'/../exam_succes/storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -33,6 +37,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 require __DIR__.'/../vendor/autoload.php';
 
+// require __DIR__.'/../exam_succes/vendor/autoload.php';
+
 /*
 |--------------------------------------------------------------------------
 | Run The Application
@@ -45,6 +51,8 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+// $app = require_once __DIR__.'/../exam_succes/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
